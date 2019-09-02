d6e1cfe07286328ef31099d01a2cbba0b88868c4
IGOR DOS SANTOS SALGADO <isantos@br.ibm.com>
2019-09-02 13:40:21 -0400

    geocode do not get required...

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,5 @@
+const geocode = require('../../src/utils/geocode')
+
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 const messageOne = document.querySelector('#message-1');
@@ -39,7 +41,22 @@ sendLocationButton.addEventListener('click', () => {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         }
-        console.log(position);
+
+        const test = geocode(location, callback);
+       console.log(test)
+        fetch('/weather?address=' + location).then((response) => {
+            response.json().then((data) => {
+                if (data.error) {
+                    messageOne.textContent = data.error;
+                    messageTwo.textContent = '';
+                } else {
+                    messageOne.textContent = data.location;
+                    messageTwo.textContent = data.forecast;
+                };
+            });
+            console.log(location)
+        });
+
     });
 });
 
